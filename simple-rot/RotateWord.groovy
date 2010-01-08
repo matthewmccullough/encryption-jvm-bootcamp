67fbb66060d1d2184674d201f8578d0104a795c8
Matthew McCullough <matthewm@ambientideas.com>
2010-01-08 11:51:02 -0700

    Cleanup of the code, variable names, comments, debug output

--- simple-rot/RotateWord.groovy
+++ simple-rot/RotateWord.groovy
@@ -1,50 +1,62 @@
+/**
+ * A simple rotation ciper helper.
+ *
+ * USAGE: groovy RotateWord.groovy <yourword>
+ */
 public class RotateWord {
   public static void main (String[] args) {
-    //char[] originalword = args[0]
-    println "Rot(-3)  Word: " + encode(args[0], -3)
-    println "Rot(-2)  Word: " + encode(args[0], -2)
-    println "Rot(-1)  Word: " + encode(args[0], -1)
-    println "Original Word: ${args[0]}"
-    println "Rot(1)   Word: " + encode(args[0], 1)
-    println "Rot(2)   Word: " + encode(args[0], 2)
-    println "Rot(3)   Word: " + encode(args[0], 3)
-    println "Rot(4)   Word: " + encode(args[0], 4)
-    println "Rot(5)   Word: " + encode(args[0], 5)
-    println "Rot(6)   Word: " + encode(args[0], 6)
-    println "Rot(7)   Word: " + encode(args[0], 7)
+    String originalword = args[0]
+    println "Rot(-3)  Word: " + rotateAllChars(originalword, -3)
+    println "Rot(-2)  Word: " + rotateAllChars(originalword, -2)
+    println "Rot(-1)  Word: " + rotateAllChars(originalword, -1)
+    println "Original Word: ${originalword}"
+    println "Rot(1)   Word: " + rotateAllChars(originalword, 1)
+    println "Rot(2)   Word: " + rotateAllChars(originalword, 2)
+    println "Rot(3)   Word: " + rotateAllChars(originalword, 3)
+    println "Rot(4)   Word: " + rotateAllChars(originalword, 4)
+    println "Rot(5)   Word: " + rotateAllChars(originalword, 5)
+    println "Rot(6)   Word: " + rotateAllChars(originalword, 6)
+    println "Rot(7)   Word: " + rotateAllChars(originalword, 7)
   }
   
-  public static String encode(String plainText, int rotationAmount) {
-    // deal with the case that method is called with null argument
-    if (plainText == null) return plainText;
+  /**
+   * Rotate the entire String by the specified rotation amount.
+   */
+  public static String rotateAllChars(String plainText, int rotationAmount) {
+    if (plainText == null)
+      throw new IllegalArgumentException("plainText can't be null")
 
-    // encode plainText
-    String encodedMessage = "";
+    String encodedMessage = ""
+    
+    //Loop through each character in the plaintext, rotating each one
     for (int i = 0; i < plainText.length(); i++) {
-      char c = plainText.toLowerCase().charAt(i);
-      encodedMessage += rotateChar(c, rotationAmount);
+      //TODO: Improve to handle upper and lower case letters
+      char c = plainText.toLowerCase().charAt(i)
+      encodedMessage += rotateChar(c, rotationAmount)
     }
-    return encodedMessage;
+    
+    return encodedMessage
   }
   
+  /**
+   * Rotate one character by the specified amount
+   */
   private static char rotateChar(char c, int rotationAmount) {
-    
     //a == 97
     //z == 122
     int num = (int)c
     int rotated = num + rotationAmount
-    int adjusted = 0;
-//    println "Num: ${num}"
-//    println "Rotated: ${rotated}"
+    int adjusted
+    
+    //Handle roll-around wrapping
     if (rotated > 122)
       adjusted = rotated - 26
     else if (rotated < 97)
       adjusted = rotated + 26
     else
       adjusted = rotated
+    
     char adjustedChar = (char)adjusted
-//    println "Adjusted: ${adjusted}"
-//    println "Returning: " + adjustedChar
     return adjustedChar
   }
 }
