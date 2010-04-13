75eddfc8fc079a24f3631e8c59362de83474ab63
Matthew McCullough <matthewm@ambientideas.com>
2010-04-13 12:10:40 -0600

    Adding salt example

--- message-digest/src/main/java/com/ambientideas/MessageDigestSHA.java
+++ message-digest/src/main/java/com/ambientideas/MessageDigestSHA.java
@@ -26,6 +26,15 @@ public class MessageDigestSHA
       + shaAndBase64Encode(message1));
     System.out.println("Message2 SHA1 digest: "
       + shaAndBase64Encode(message2));
+    System.out.println("\r\n");
+    
+    //Original account creation
+    Login loginSavedToDB = shaAndRandomSaltNewAccount("12345");
+    System.out.println("Hash: "+ loginSavedToDB.hash);
+    System.out.println("Salt: " + loginSavedToDB.salt);
+    
+    //Attempt to re-login
+    System.out.println("Rehashed: " + shaWithKnownSalt("12345", loginSavedToDB.salt));
   }
   
   /**
@@ -35,11 +44,33 @@ public class MessageDigestSHA
   public static String shaAndBase64Encode(String message) throws NoSuchAlgorithmException {
     MessageDigest sha = MessageDigest.getInstance("SHA-1");
     
-    //Salt could be applied here
-    //Integer salt = <some random number generator>
-    //sha.update(salt.getBytes());
-    
     byte[] digest = sha.digest(message.getBytes());
     return new sun.misc.BASE64Encoder().encode(digest);
+  }
+  
+  /**
+   * Derivative of the sha encoding function that allows for a known salt to
+   * be used in combination with the password
+   * @param message
+   * @param salt
+   * @return
+   * @throws NoSuchAlgorithmException
+   */
+  public static String shaWithKnownSalt(String password, String salt) throws NoSuchAlgorithmException {
+    return shaAndBase64Encode(password + salt);
+  }
+  
+  public static Login shaAndRandomSaltNewAccount(String password) throws NoSuchAlgorithmException {
+    Login acct = new Login();
+    
+    acct.salt = new String("" + new java.util.Random().nextInt());
+    acct.hash = shaWithKnownSalt(password, acct.salt);
+    
+    return acct;
+  }
+  
+  private static class Login {
+    public String hash;
+    public String salt;
   }
 }
