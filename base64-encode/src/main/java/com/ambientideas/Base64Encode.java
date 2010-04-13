7334c6da7e1e590b035a3a8abde2d92055340e52
Matthew McCullough <matthewm@ambientideas.com>
2010-04-13 11:16:34 -0600

    Example of base64 encoding

--- base64-encode/src/main/java/com/ambientideas/Base64Encode.java
+++ base64-encode/src/main/java/com/ambientideas/Base64Encode.java
@@ -1,21 +1,9 @@
 package com.ambientideas;
 
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 
 import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
 
 /**
  * Use the SecureRandom java security class to generate
@@ -23,34 +11,20 @@ import sun.misc.BASE64Encoder;
  */
 public class Base64Encode 
 {
-  public static void main( String[] args ) throws NoSuchAlgorithmException, NoSuchProviderException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
-  {
-    final String message1 = "Four score and seven years ago";
+  public static void main( String[] args ) throws IOException {
+    final String message1 = "This is <bold>code</bold> with \t tabs, semicolons;"
+    + " and alert-bells \b that could cause escaping issues if not encoded in Base64";
     
-    // Generate the Key Pair
-    final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-    
-    final SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-    keyGen.initialize(1024, random);
-    
-    KeyPair pair = keyGen.generateKeyPair();
-    
-    final PrivateKey privKey = pair.getPrivate();
-    final PublicKey pubKey = pair.getPublic();
-    
-    
-    //Encrypt using the private key
-    Cipher rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    rsa.init(Cipher.ENCRYPT_MODE, privKey);
-    byte[] encryptedBytes = rsa.doFinal(message1.getBytes());
+    System.out.println("Raw text:\r\n" + message1 + "\r\n");
     BASE64Encoder b64e = new sun.misc.BASE64Encoder();
-    String base64Encrypted = b64e.encode(encryptedBytes);
-    System.out.println("Encrypted text: " + base64Encrypted);
+    String base64Encoded = b64e.encode(message1.getBytes());
+
+    System.out.println("Base64 encoded text:\r\n" + base64Encoded + "\r\n");
     
-    //Decrypt using the private key
-    rsa.init(Cipher.DECRYPT_MODE, pubKey);
-    byte[] decryptedBytes = rsa.doFinal(encryptedBytes);
-    String decryptedText = new String(decryptedBytes);
-    System.out.println("Decrypted text: " + decryptedText);
+    //System.out.println("Base64 Decoded text: " + decryptedText);
+    BASE64Decoder b64d = new sun.misc.BASE64Decoder();
+    String base64Decoded = new String(b64d.decodeBuffer(base64Encoded));
+    
+    System.out.println("Decoded text:\r\n" + base64Decoded);
   }
 }
