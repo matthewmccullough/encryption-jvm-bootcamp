032ef19ac86014b9ec4840d61f0e662b3407e5d1
Matthew McCullough <matthewm@ambientideas.com>
2010-05-20 00:42:06 -0600

    Skeleton for delay demo

---
+++ delay/src/main/java/com/ambientideas/DelayLogin.java
@@ -0,0 +1,18 @@
+package com.ambientideas;
+
+import java.io.IOException;
+
+/**
+ * Use the SecureRandom java security class to generate
+ * a more expensive, but cryptographically secure random number.
+ */
+public class DelayLogin 
+{
+  public static void main( String[] args ) throws IOException {
+    //Static map for username and incorrect login attempts
+	//Prompt for username
+	//prompt for password
+	//If previously incorrect, delay by this amount before allowing
+	//another login attempt
+  }
+}
