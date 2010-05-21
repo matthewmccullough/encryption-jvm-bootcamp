ea30db1a7c4bc2aa7d002d8379a1778f7331057f
Matthew McCullough <matthewm@ambientideas.com>
2010-05-21 08:49:24 -0600

    Simple RSA example

--- rsa-simple-algorithm/src/main/java/com/ambientideas/RSASimple.java
+++ rsa-simple-algorithm/src/main/java/com/ambientideas/RSASimple.java
@@ -1,13 +1,16 @@
 package com.ambientideas;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 // http://www.cs.princeton.edu/introcs/79crypto/RSA.java.html
-public class RSA {
+public class RSASimple {
     private static BigInteger one = new BigInteger("1");
     private static SecureRandom random = new SecureRandom();
     private BigInteger privateKey, publicKey, modulus;
 
     // Generate an N-bit (roughly) public and private key
-    RSA(int n) {
+    RSASimple(int n) {
         BigInteger p = BigInteger.probablePrime(n/2, random);
         BigInteger q = BigInteger.probablePrime(n/2, random);
         BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
@@ -25,11 +28,20 @@ public class RSA {
     }
 
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]); // key size
-        RSA key = new RSA(n);
+        int n = 2048;	
+    	    if (args.length > 0 && args[0] != null) {
+    	    		n = Integer.parseInt(args[0]); // key size
+    	    }
+        RSASimple key = new RSASimple(n);
+        
         // create random message, encrypt and decrypt
         BigInteger message = new BigInteger(n-1, random);
+        System.out.println("Plaintext: " + message);
+        
         BigInteger encrypt = key.encrypt(message);
+        System.out.println("Encrypted: " + encrypt);
+        
         BigInteger decrypt = key.decrypt(encrypt);
+        System.out.println("Decrypted: " + decrypt);
     }
 }
