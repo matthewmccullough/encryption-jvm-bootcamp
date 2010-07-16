b4067d98dfb9d6f8191dc499e2dcc5b608300ccf
Matthew McCullough <matthewm@ambientideas.com>
2010-07-15 19:46:16 -0500

    Adding timing to show off the delta between random and securerandom

--- secure-random/src/main/java/com/ambientideas/SecureRandomNumber.java
+++ secure-random/src/main/java/com/ambientideas/SecureRandomNumber.java
@@ -2,6 +2,7 @@ package com.ambientideas;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Random;
 
 /**
  * Use the SecureRandom java security class to generate
@@ -11,13 +12,44 @@ public class SecureRandomNumber
 {
   public static void main( String[] args ) throws NoSuchAlgorithmException
   {
-    //Do the expensive one time setup of the
-    // random number generator instance
-    SecureRandom prng = SecureRandom.getInstance("SHA1PRNG");
+	int iterations = 10000;
+	
+	random(iterations);
+	prng(iterations);
+  }
+
+  private static void random(int iterations) throws NoSuchAlgorithmException {
+	long beforeRandom = java.lang.System.currentTimeMillis();
+	int randomNum = 0;
+	for (int i = 0; i < iterations; i++) {
+	    //Setup the random number generator instance
+	    Random random = new java.util.Random();
+	    
+	    //Get the next random number
+	    randomNum = random.nextInt();
+	}
     
-    //Get the next random number
-    String randomNum = new Integer( prng.nextInt() ).toString();
+    long afterRandom = java.lang.System.currentTimeMillis();
     
-    System.out.println("Random number: " + randomNum);
+    //System.out.println("Cheap random number: " + randomNum);
+    System.out.println("Cheap random number generation time in millis: " + (afterRandom - beforeRandom) + " Iterations: " + iterations);
+  }
+
+  private static void prng(int iterations) throws NoSuchAlgorithmException {
+	long beforePRNG = java.lang.System.currentTimeMillis();
+	int randomNum = 0;
+	for (int i = 0; i < iterations; i++) {
+	    //Do the expensive one time setup of the
+	    // random number generator instance
+	    SecureRandom prng = SecureRandom.getInstance("SHA1PRNG");
+	    
+	    //Get the next random number
+	    randomNum =  prng.nextInt();
+	}
+    
+    long afterPRNG = java.lang.System.currentTimeMillis();
+    
+    //System.out.println("PRNG random number: " + randomNum);
+    System.out.println("PRNG random number generation time in millis: " + (afterPRNG - beforePRNG) + " Iterations: " + iterations);
   }
 }
