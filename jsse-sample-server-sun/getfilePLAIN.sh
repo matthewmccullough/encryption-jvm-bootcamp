bed0f8c8432382543fc3572688529011db141812
Matthew McCullough <matthewm@ambientideas.com>
2010-07-16 07:19:43 -0500

    Adding plain and SSL serving and getting

---
+++ jsse-sample-server-sun/getfilePLAIN.sh
@@ -0,0 +1,2 @@
+#Retrieves the test file from the server
+curl -k http://localhost:2001/samplemessage.txt
---
+++ jsse-sample-server-sun/servefileSSL.sh
@@ -0,0 +1,2 @@
+#Serves up all files in the current directory over port 2001 via TLS
+java ClassFileServer 2001 .
