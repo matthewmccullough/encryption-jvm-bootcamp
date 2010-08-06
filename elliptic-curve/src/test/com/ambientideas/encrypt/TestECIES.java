65be74da3b996bc1085eee6f5b60c4b4258f7ca8
Matthew McCullough <matthewm@ambientideas.com>
2010-08-06 12:06:54 -0600

    Adding unit test for ECIES elliptic curve crypto

---
+++ elliptic-curve/src/test/com/ambientideas/encrypt/TestECIES.java
@@ -0,0 +1,14 @@
+package com.ambientideas.encrypt;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestECIES
+{
+	@Test
+    public void testECIES() throws Exception {
+        ExampleECIES.main(null);
+    }
+}
