6b6b8ae0ed8c7a8bb6df4291bb37a693130b9592
Matthew McCullough <matthewm@ambientideas.com>
2010-08-27 01:07:27 -0400

    Example to read from a keystore

---
+++ keystore-symmetric-encrypt/src/test/java/com/ambientideas/TestSymmetric.java
@@ -0,0 +1,14 @@
+package com.ambientideas;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestSymmetric
+{
+	@Test
+    public void testAES() throws Exception {
+        SymmetricEncryptAES.main(null);
+    }
+}
