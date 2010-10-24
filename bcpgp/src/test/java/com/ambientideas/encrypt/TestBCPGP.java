20ea3d8f59fc1a81b79949b4e318d98829c00601
Matthew McCullough <matthewm@ambientideas.com>
2010-10-23 20:33:30 -0600

    Adding Bouncy Castle PGP example

---
+++ bcpgp/src/test/java/com/ambientideas/encrypt/TestBCPGP.java
@@ -0,0 +1,14 @@
+package com.ambientideas.encrypt;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestBCPGP
+{
+	@Test
+    public void testECIES() throws Exception {
+        ExampleBCPGP.main(null);
+    }
+}
