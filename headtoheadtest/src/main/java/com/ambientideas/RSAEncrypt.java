91270da451a7de2cdf85a4caed633fea6e2628ec
Matthew McCullough <matthewm@ambientideas.com>
2010-11-04 17:09:02 -0600

    Updating the flow of the RSA example to be more comprehensible, straightforward, and readable in a classroom session

--- headtoheadtest/src/main/java/com/ambientideas/RSAEncrypt.java
+++ headtoheadtest/src/main/java/com/ambientideas/RSAEncrypt.java
@@ -13,59 +13,68 @@ import sun.misc.BASE64Encoder;
 
 public class RSAEncrypt
 {
-	private static final int RSA_BITSTRENGTH = 3072;
-	private KeyPair keyPair;
+    private static final int RSA_BITSTRENGTH = 3072;
+    private KeyPair keyPair;
 
-	public RSAEncrypt() throws Exception
-	{
-		initialize();
-	}
+    public RSAEncrypt() throws Exception
+    {
+        initialize();
+    }
 
-	public void initialize() throws Exception
-	{
-		KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
-		keygen.initialize(RSA_BITSTRENGTH);
-		keyPair = keygen.generateKeyPair();
-	}
+    public void initialize() throws Exception
+    {
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+        keygen.initialize(RSA_BITSTRENGTH);
+        keyPair = keygen.generateKeyPair();
+    }
 
     public static long runExample() throws Exception {
-    		long beforeRSA = java.lang.System.currentTimeMillis();
-    	
-        RSAEncrypt app = new RSAEncrypt();
-
+        long beforeRSA = java.lang.System.currentTimeMillis();
+        
         FileReader fReader = new FileReader(HeadToHeadTest.CLEARTEXT_FILENAME);
         java.io.BufferedReader bReader = new java.io.BufferedReader(fReader);
-        String input = bReader.readLine();
+        String plaintext = bReader.readLine();
         
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+        keygen.initialize(2048);
+        KeyPair keyPair = keygen.generateKeyPair();
         
-        String ciphertext = app.encrypt(input);
-        app.decrypt(ciphertext);      
-		long afterRSA = java.lang.System.currentTimeMillis();
+        String ciphertext = null;
+        
+        PublicKey key = keyPair.getPublic();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        byte[] cipherbytes = cipher.doFinal(plaintext.getBytes("UTF8"));
+        ciphertext = encodeBASE64(cipherbytes);
+        
+        long afterRSA = java.lang.System.currentTimeMillis();
 
         return afterRSA - beforeRSA;
     }
 
-	public String encrypt(String plaintext)  throws Exception
-	{
-		PublicKey key = keyPair.getPublic();
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    public String encrypt(String plaintext)  throws Exception
+    {
+        PublicKey key = keyPair.getPublic();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-		cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
 
-		byte[] ciphertext = cipher.doFinal(plaintext.getBytes("UTF8"));
-		return encodeBASE64(ciphertext);
-	}
+        byte[] ciphertext = cipher.doFinal(plaintext.getBytes("UTF8"));
+        return encodeBASE64(ciphertext);
+    }
 
-	public String decrypt(String ciphertext)  throws Exception
-	{
-		PrivateKey key = keyPair.getPrivate();
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    public String decrypt(String ciphertext)  throws Exception
+    {
+        PrivateKey key = keyPair.getPrivate();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-		cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.DECRYPT_MODE, key);
 
-		byte[] plaintext = cipher.doFinal(decodeBASE64(ciphertext));
-		return new String(plaintext, "UTF8");
-	}
+        byte[] plaintext = cipher.doFinal(decodeBASE64(ciphertext));
+        return new String(plaintext, "UTF8");
+    }
 
     private static String encodeBASE64(byte[] bytes)
     {
@@ -77,22 +86,22 @@ public class RSAEncrypt
     {
         BASE64Decoder b64 = new BASE64Decoder();
         return b64.decodeBuffer(text);
-    }	
+    }    
 
-	public static void main(String[] args) throws Exception
-	{
-		RSAEncrypt app = new RSAEncrypt();
+    public static void main(String[] args) throws Exception
+    {
+        RSAEncrypt app = new RSAEncrypt();
 
-		System.out.println("Enter a line: ");
-		java.io.InputStreamReader sreader = new java.io.InputStreamReader(System.in);
-		java.io.BufferedReader breader = new java.io.BufferedReader(sreader);
-		String input = breader.readLine();
+        System.out.println("Enter a line: ");
+        java.io.InputStreamReader sreader = new java.io.InputStreamReader(System.in);
+        java.io.BufferedReader breader = new java.io.BufferedReader(sreader);
+        String input = breader.readLine();
 
-		System.out.println("Plaintext = " + input);
+        System.out.println("Plaintext = " + input);
 
-		String ciphertext = app.encrypt(input);
-		System.out.println("After Encryption Ciphertext = " + ciphertext);
-		System.out.println("After Decryption Plaintext = " + app.decrypt(ciphertext));
+        String ciphertext = app.encrypt(input);
+        System.out.println("After Encryption Ciphertext = " + ciphertext);
+        System.out.println("After Decryption Plaintext = " + app.decrypt(ciphertext));
 
-	}
+    }
 }
