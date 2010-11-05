9637dea575b599ce96d868c949180da327f4340d
Matthew McCullough <matthewm@ambientideas.com>
2010-11-05 10:27:17 -0400

    Adding a simple Jasypt example

--- jasypt-example/src/main/java/com/ambientideas/encryption/App.java
+++ jasypt-example/src/main/java/com/ambientideas/encryption/App.java
@@ -1,5 +1,8 @@
 package com.ambientideas.encryption;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
 /**
  * Hello world!
  *
@@ -7,7 +10,35 @@ package com.ambientideas.encryption;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {        
+        testDefaultAlgorithm();
+        testPBEWithMD5AndDES();
+        testPBEWithMD5AndTripleDES();
     }
+
+	private static void testDefaultAlgorithm() {
+		StandardPBEStringEncryptor se1 = new StandardPBEStringEncryptor();
+        se1.setPassword("thisissupersecret");
+        
+        String encryptedString1 = se1.encrypt("Hello World");
+        System.out.println("Encrypted string with default algorithm is: " + encryptedString1);
+	}
+
+	private static void testPBEWithMD5AndDES() {
+		StandardPBEStringEncryptor se2 = new StandardPBEStringEncryptor();
+        se2.setAlgorithm("PBEWithMD5AndDES");
+        se2.setPassword("thisissupersecret");
+        
+        String encryptedString2 = se2.encrypt("Hello World");
+        System.out.println("Encrypted string with PBEWithMD5AndDES is: " + encryptedString2);
+	}
+	
+	private static void testPBEWithMD5AndTripleDES() {
+		StandardPBEStringEncryptor se2 = new StandardPBEStringEncryptor();
+        se2.setAlgorithm("PBEWithMD5AndTripleDES");
+        se2.setPassword("thisissupersecret");
+        
+        String encryptedString2 = se2.encrypt("Hello World");
+        System.out.println("Encrypted string with PBEWithMD5AndDES is: " + encryptedString2);
+	}
 }
