91ac097b915d534ffee91d8150620393803f9a63
Matthew McCullough <matthewm@ambientideas.com>
2011-03-03 21:40:04 -0700

    Bouncy castle DES example

---
+++ bc-DES/src/test/java/com/ambientideas/encrypt/TestBCDES.java
@@ -0,0 +1,14 @@
+package com.ambientideas.encrypt;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestBCDES
+{
+	@Test
+    public void testECIES() throws Exception {
+        ExampleBCDES.main(null);
+    }
+}
