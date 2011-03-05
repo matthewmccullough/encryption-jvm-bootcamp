4a4684670d83937b23b94a05c0b3d5555f5ef018
Matthew McCullough <matthewm@ambientideas.com>
2011-03-05 08:34:42 -0600

    More variations on Jasypt hashing

--- jasypt-hash/src/main/java/com/ambientideas/encrypt/ExampleJasyptHash.java
+++ jasypt-hash/src/main/java/com/ambientideas/encrypt/ExampleJasyptHash.java
@@ -5,21 +5,41 @@ import org.jasypt.digest.StandardStringDigester;
 /*
  * Use BouncyCastle for SHA Hashing
  */
-public class ExampleJasyptHash {
-    
+public class ExampleJasyptHash {  
 	public static void main(String[] args) throws Exception {
 		//Register Bouncy Castle JCE provider
 		final String DATA = "Four score and seven years ago.";
 		
-		System.out.println(hash(DATA));
+		System.out.println("Plaintext: " + DATA);
+		System.out.println("Hash First: " + hash(DATA));
+		System.out.println("Hash Again: " + hash(DATA));
 	}
 	
 	public static String hash(String data) {
 	    StandardStringDigester digester = new StandardStringDigester();
-        digester.setAlgorithm("SHA-512"); // optionally set the algorithm
-        digester.setIterations(50000);
+	    digester.setSaltSizeBytes(0);
         String digest = digester.digest(data);
 	    
 	    return digest;
 	}
+	
+	   public static String hashSHA512(String data) {
+	        StandardStringDigester digester = new StandardStringDigester();
+	        digester.setSaltSizeBytes(0);
+	        digester.setAlgorithm("SHA-512");
+//	        digester.setIterations(50000);
+	        String digest = digester.digest(data);
+	        
+	        return digest;
+	    }
+	   
+       public static String hashSHA512By50000(String data) {
+           StandardStringDigester digester = new StandardStringDigester();
+           digester.setSaltSizeBytes(0);
+           digester.setAlgorithm("SHA-512");
+           digester.setIterations(50000);
+           String digest = digester.digest(data);
+           
+           return digest;
+       }
 }
--- jasypt-hash/src/test/java/com/ambientideas/encrypt/TestJasyptHash.java
+++ jasypt-hash/src/test/java/com/ambientideas/encrypt/TestJasyptHash.java
@@ -7,17 +7,27 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class TestJasyptHash
-{
+public class TestJasyptHash {
     public static String DATA = "Four score and seven years ago.";
-    
-	@Test
-    public void testBCDES() throws Exception {
-        Assert.assertEquals("HSKASDLKSALKJD", ExampleJasyptHash.hash(DATA));
-    }
-	
+
     @Test
-    public void testBCBlowfish() throws Exception {
-        Assert.assertEquals("SHSDHSDHJKS", ExampleJasyptHash.hash(DATA));
+    public void testHashConsistencyBetweenRuns() throws Exception {
+        final String expectedHash = ExampleJasyptHash.hash(DATA);
+        String hashAgain = ExampleJasyptHash.hash(DATA);
+        Assert.assertEquals(expectedHash, hashAgain);
+    }
+
+    @Test
+    public void testHashSHA512ConsistencyBetweenRuns() throws Exception {
+        final String expectedHash = ExampleJasyptHash.hashSHA512(DATA);
+        String hashAgain = ExampleJasyptHash.hashSHA512(DATA);
+        Assert.assertEquals(expectedHash, hashAgain);
+    }
+
+    @Test
+    public void testHash50000ConsistencyBetweenRuns() throws Exception {
+        final String expectedHash = ExampleJasyptHash.hashSHA512By50000(DATA);
+        String hashAgain = ExampleJasyptHash.hashSHA512By50000(DATA);
+        Assert.assertEquals(expectedHash, hashAgain);
     }
 }
